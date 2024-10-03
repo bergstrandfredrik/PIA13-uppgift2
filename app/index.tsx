3c89495d670f2e9c9e1179cdd66f190cd9157e57
Fredrik Bergstrand <fred.bergstrand@gmail.com>
2024-10-03 05:01:28 +0200

    Complete task 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter () {
+    setCounter(counter + 1);
+  }
+  function decrementCounter () {
+    if(counter === 0) return;
+    setCounter(counter - 1);
+  }
+  function resetCounter () {
+    setCounter(0);
+  }
+
+  
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={{flex: 2}}>
+        <Text style={{fontSize: 50}}>{counter}</Text>
+      </View>
+      <View style={{}}>
+        <Button title="plus" onPress={incrementCounter}/>
+      </View>
+      <View >
+        <Button title="minus" onPress={decrementCounter}/>
+      </View>
+      { counter > 0 &&
+        <View>
+          <Button title="reset" onPress={resetCounter}/>
+        </View>
+      }
+      <View style={{flex: 10}}></View>
     </View>
   );
 }
